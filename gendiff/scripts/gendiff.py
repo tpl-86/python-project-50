9b0af8fd7122bab8e1fc150371c954262d920209
tpl-86 <tron-050686@mail.ru>
2024-03-09 17:54:57 +0300

    Add __init.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,5 @@
-import argparse
 #!/usr/bin/env python3
+import argparse
 
 
 parser = argparse.ArgumentParser(
@@ -8,6 +8,8 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument('-f', '--format',
+                    help='set format of output')
 
 args = parser.parse_args()
 print(args)

0a271f4137853ec76079e13a8e029ca3c206f5fb
tpl-86 <tron-050686@mail.ru>
2024-03-10 21:36:12 +0300

    Add generate_diff.py and working_dir

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 import argparse
+from gendiff.generate_diff import generate_diff
 
 
 parser = argparse.ArgumentParser(
         prog="gendiff",
         description="Compares two configuration files and shows a difference.")
 
-parser.add_argument('first_file')
-parser.add_argument('second_file')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
 parser.add_argument('-f', '--format',
                     help='set format of output')
 
 args = parser.parse_args()
-print(args)
 
+
+def main():
+    print(generate_diff(args.first_file, args.second_file))
+
+
+if __name__ == '__main__':
+    main()

1eaa9f8de629de137f77d4f569d00193e904bc65
tpl-86 <tron-050686@mail.ru>
2024-03-13 13:43:26 +0300

    Change generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,24 +1,27 @@
 import json
 
 
-def get_value_diff(value, values1, values2):
-    if value in values1 and value in values2:
-        if values1[value] == values2[value]:
-            return f'    {value}: {values1.get(value)}\n'
-        else:
-            return f'  - {value}: {values1.get(value)}\n'
-        f'  + {value}: {values2.get(value)}\n'
-    elif value not in values2:
-        return f'  - {value}: {values1.get(value)}\n'
-    else:
-        return f'  + {value}: {values2.get(value)}\n'
+def is_value_diff(value, values):
+    if value in values:
+        return True
 
 
 def generate_diff(file1, file2):
     result = ''
     value1 = json.load(open(file1))
     value2 = json.load(open(file2))
-    union_values = sorted(list(set(value1.keys()) | set(value2.keys())))
+    union_values = sorted(list(set(value1.keys()) | set (value2.keys())))
+    inter_values = set(value1.keys()) & set (value2.keys())
+    diff_values1 = set(value1.keys()) - set(value2.keys())
     for i in union_values:
-        result += get_value_diff(i, value1, value2)
+        if is_value_diff(i, inter_values):
+            if value1[i] == value2[i]:
+                result += f'    {i}: {value1.get(i)}\n'
+            else:
+                result += f'  - {i}: {value1.get(i)}\n'
+                result += f'  + {i}: {value2.get(i)}\n'
+        elif is_value_diff(i, diff_values1):
+            result += f'  - {i}: {value1.get(i)}\n'
+        else:
+            result += f'  + {i}: {value2.get(i)}\n'
     return f'{{\n{result}}}\n'.lower()
